8d3b1dd28485a6565738549d93f34452ea8ecd5f
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-03-31 15:37:49 +0100

    Init

--- Basics/helloWorld.c
+++ Basics/helloWorld.c
@@ -1,1 +1,7 @@
 #include <stdio.h> // Include standard io library
+
+int main() // Main function
+{
+    printf("Hello World!"); // Print "Hello World!" to the console
+    return 0; // Return 0 to the OS
+}
